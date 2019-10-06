ef1523627644f3b86b4cdba10b9cd57c9344d52f
Chris White <chris@prefect.io>
2019-10-05 17:22:51 -0700

    Store DB in home dir so its absolute path is known

--- init.sh
+++ init.sh
@@ -11,13 +11,14 @@ source ~/.bashrc
 sudo apt-get update -y
 sudo apt-get install gcc -y
 
+# download DB of IP data
+curl -fL https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz -o ~/geocities.tar.gz
+cd ~
+tar -xzf geocities.tar.gz # creates file ~/GeoLite2-City_20191001/GeoLite2-City.mmdb
+
 # create conda environment for example
 conda create -n ssh-example python=3.7 -y
 git clone https://github.com/cicdw/ssh-etl-monitoring.git
 cd ssh-etl-monitoring
 source activate ssh-example
 pip install -r requirements.txt
-
-# download DB of IP data
-curl -fL https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz -o geocities.tar.gz
-tar -xzf geocities.tar.gz # creates file GeoLite2-City_20191001/GeoLite2-City.mmdb
